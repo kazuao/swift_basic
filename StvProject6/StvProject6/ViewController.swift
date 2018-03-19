0cb12086be63959df209222e7a536833356a4793
kazua <kazua.kuro@gmail.com>
2018-03-19 15:59:01 +0900

    [Fixed] #2.1.1の課題が終了しました。

---
+++ StvProject6/StvProject6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StvProject6
+//
+//  Created by kazua on 2018/03/19.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private var changeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        changeLabel.text = NSLocalizedString("Objective-C lecture started", comment: "")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
