d02f0fc77276491b59ddd5a3828d666b31739438
kazua <kazua.kuro@gmail.com>
2018-03-25 08:17:17 +0900

    [Fix] #2.1.10の課題を修正しました。
    SwiftLint対応しました。

--- StvProject15/StvProject15/ViewController.swift
+++ StvProject15/StvProject15/ViewController.swift
@@ -10,31 +10,37 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // outlet接続
     @IBOutlet private weak var tableView: UITableView!
     
+    // 宣言
     var catImg = #imageLiteral(resourceName: "Image1")
     var dogImg = #imageLiteral(resourceName: "Image2")
     var pigImg = #imageLiteral(resourceName: "Image3")
     var animalsImage = [UIImage]()
-    var animalsArr = [String?]()
+    var animalsArr = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // delegate / datasource
         tableView.delegate = self
         tableView.dataSource = self
-        
+
+        // 配列化
         animalsImage = [catImg, dogImg, pigImg]
         
-        let filePath = Bundle.main.path(forResource: "Property List", ofType: "plist")
-        
-        let plist = NSDictionary(contentsOfFile: filePath!)
-        
-        if plist!["animals"] != nil {
-            let animalsArray = plist!["animals"]
-            self.animalsArr = (animalsArray as? [String])!
-            print(animalsArr)
+        // plistから取得する
+        guard let filePath = Bundle.main.path(forResource: "Property List", ofType: "plist") else {
+            return
         }
+        guard let plist = NSDictionary(contentsOfFile: filePath) else {
+            return
+        }
+        guard let animalsArray = plist["animals"] as? [String] else {
+            return
+        }
+        self.animalsArr = animalsArray
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,13 +48,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell =
-            tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? SampleTableViewCell
         
-        cell?.tableLabel.text = self.animalsArr[indexPath.row]
-        cell?.tableImage.image = self.animalsImage[indexPath.row]
+         guard let cell =
+            tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? SampleTableViewCell else {
+                
+                fatalError("TweetCell is nil.")
+        }
+        cell.tableLabel.text = self.animalsArr[indexPath.row]
+        cell.tableImage.image = self.animalsImage[indexPath.row]
         
-        return cell!
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
