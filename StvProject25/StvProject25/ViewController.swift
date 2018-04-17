9ad541765fe3c7422ebdfa8b2aa6bb441be886ef
kazua <kazua.kuro@gmail.com>
2018-04-17 19:09:18 +0900

    [Fixed] #2.1.1の課題が終了しました。

---
+++ StvProject25/StvProject25/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StvProject25
+//
+//  Created by kazua on 2018/04/17.
+//  Copyright © 2018年 kazua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
