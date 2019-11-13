d47aff7730f61fefe1603faecee2eb1fe77f71f6
kazua <kazua.kuro@gmail.com>
2019-11-13 15:18:46 +0900

    [refactor]

--- StvProject9/StvProject9/ViewController.swift
+++ StvProject9/StvProject9/ViewController.swift
@@ -12,23 +12,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     @IBAction func showAlertBtn(_ sender: Any) {
         
-        let alert: UIAlertController =
-            UIAlertController(title: "Alert!", message: "Sample Alert!", preferredStyle: .alert)
+        let alert: UIAlertController = UIAlertController(title: "あらーとだよ",
+                                                        message: "シンプルなアラートの実装です",
+                                                        preferredStyle: .alert)
         
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: .default) { (_: UIAlertAction) -> Void in }
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK",
+                                                         style: .default,
+                                                         handler: { (_: UIAlertAction) -> Void in
+            self.okAlertButtonTupped()
+        })
+        
+        let cancelAlertAction: UIAlertAction = UIAlertAction(title: "Cancel",
+                                                             style: .cancel,
+                                                             handler: { (_: UIAlertAction) -> Void in
+            self.cancelAleratButtonTupped()
+        })
         
         alert.addAction(defaultAction)
+        alert.addAction(cancelAlertAction)
         
         self.present(alert, animated: true, completion: nil)
     }
     
+    private func okAlertButtonTupped() {
+        print("おーけー")
+    }
+    
+    private func cancelAleratButtonTupped() {
+        print("キャンセル")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
