2e0c0be7de8ff3466efb2e507b8647e5a7baa0ec
kazua <kazua.kuro@gmail.com>
2019-11-14 14:35:44 +0900

    [refactor]

--- StvProject12/StvProject12/ViewController.swift
+++ StvProject12/StvProject12/ViewController.swift
@@ -8,32 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet private weak var myTextField: UITextField!
     @IBOutlet private weak var inputText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputText.text = " "
         myTextField.delegate = self
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        self.inputText.text = myTextField.text
+        inputText.text = myTextField.text
         myTextField.resignFirstResponder()
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
         textField.resignFirstResponder()
         inputText.text = textField.text
         return true
     }
     
+    func showAlert() {
+        
+        let alert: UIAlertController =
+            UIAlertController(title: "Alert!",
+                              message: "30文字までやで",
+                              preferredStyle: .alert)
+        
+        let defaultAction: UIAlertAction =
+            UIAlertAction(title: "OK",
+                          style: .default) { (_: UIAlertAction) -> Void in }
+        
+        alert.addAction(defaultAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
     func textField(_ textField: UITextField,
                    shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
-        // wrapを安全に外す
         guard let unwrapString = self.myTextField.text as NSString? else {
             return false
         }
@@ -43,23 +68,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if newString.count > 30 {
             self.showAlert()
         }
+        
         return true
-    }
-    
-    func showAlert() {
-        
-        let alert: UIAlertController =
-            UIAlertController(title: "Alert!", message: "30文字までやで", preferredStyle: .alert)
-        
-        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: .default) { (_: UIAlertAction) -> Void in }
-        
-        alert.addAction(defaultAction)
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
     }
 }
